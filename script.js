f033b57393fef597f40fc04ab52407b97e04bcc6
Adam Blevins <blevinspercussion@gmail.com>
2022-07-22 08:15:47 -0500

    Finished creating DOM elements for detail card in script.js

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ async function populatDetailCards() {
     const data = await response.json();
 
     for (let i=0; i<data.length; i++) {
+        // Create Detail Card elements
         let detailCard = document.createElement('div');
+        let detailCardTop = document.createElement('div');
+        let detailCardImage = document.createElement('div');
+        let detailCardBottom = document.createElement('div');
+        let detailCardTitle = document.createElement('p');
+        let detailCardCurrentTime = document.createElement('h1');
+        let detailCardPreviousTime = document.createElement('p');
+        let ellipsis = document.createElement('img');
+
+        // Set id and class of elements
         detailCard.id = (`detailCard${data[i].title}`);
+        detailCard.classList.add('detail-card');
+
+        // Add elements to the DOM
+
         mainContent.appendChild(detailCard);
         
 
